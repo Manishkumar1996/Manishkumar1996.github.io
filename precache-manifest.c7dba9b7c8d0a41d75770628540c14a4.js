45a543142c217961bdf5ec12a7f44c010a903c78
Manish Kumar <manish.prajapat@hfactorial.com>
2020-08-29 23:39:57 +0530

    Updates

---
+++ precache-manifest.c7dba9b7c8d0a41d75770628540c14a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92e3a71d8944525e53912f4d83779e9",
+    "url": "/manishkumar1996.github.io/index.html"
+  },
+  {
+    "revision": "18b1a883cd529c17bd25",
+    "url": "/manishkumar1996.github.io/static/css/main.bb8c9dca.chunk.css"
+  },
+  {
+    "revision": "dbbaadc791629327342d",
+    "url": "/manishkumar1996.github.io/static/js/2.dfb926e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/manishkumar1996.github.io/static/js/2.dfb926e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18b1a883cd529c17bd25",
+    "url": "/manishkumar1996.github.io/static/js/main.57d6e39c.chunk.js"
+  },
+  {
+    "revision": "d733df8dbfdbe82382c9",
+    "url": "/manishkumar1996.github.io/static/js/runtime-main.f1473793.js"
+  }
+]);
