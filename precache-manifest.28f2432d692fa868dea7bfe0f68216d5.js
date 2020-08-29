b6b3ba2370b92bf024d3ea415ec33c4af980c9d1
Manish Kumar <manish.prajapat@hfactorial.com>
2020-08-29 21:47:20 +0530

    Updates

---
+++ precache-manifest.28f2432d692fa868dea7bfe0f68216d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761263e582ca2ed361e39831e23ab862",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "b139ed1b7d0a5a6a2dd1",
+    "url": "/dashboard/static/css/main.bb8c9dca.chunk.css"
+  },
+  {
+    "revision": "2be4acbacbbfb434ac6c",
+    "url": "/dashboard/static/js/2.eff96ffc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dashboard/static/js/2.eff96ffc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b139ed1b7d0a5a6a2dd1",
+    "url": "/dashboard/static/js/main.c96f4266.chunk.js"
+  },
+  {
+    "revision": "8f3ec870dce268db1d96",
+    "url": "/dashboard/static/js/runtime-main.2ec0fa51.js"
+  }
+]);
