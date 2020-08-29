1c9e03bf5e880898b8f4e87fb8b3c88dcafa9c58
Manish Kumar <manish.prajapat@hfactorial.com>
2020-08-29 18:09:10 +0530

    ADDED: Dashboard with build

---
+++ build/precache-manifest.0020857b6a89a00764fc7552896c8a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+    {
+        "revision": "072fc419cc5473d65dd8e4d1bb474a7e",
+        "url": "/index.html"
+    },
+    {
+        "revision": "be9f28f4e960fd4ba81c",
+        "url": "/static/css/main.372304bc.chunk.css"
+    },
+    {
+        "revision": "95b7690af16985faa905",
+        "url": "/static/js/2.a713c5c1.chunk.js"
+    },
+    {
+        "revision": "c64c486544348f10a6d6c716950bc223",
+        "url": "/static/js/2.a713c5c1.chunk.js.LICENSE.txt"
+    },
+    {
+        "revision": "be9f28f4e960fd4ba81c",
+        "url": "/static/js/main.a90ab098.chunk.js"
+    },
+    {
+        "revision": "4040b55ff6b868e876aa",
+        "url": "/static/js/runtime-main.2c219ed3.js"
+    }
+]);
